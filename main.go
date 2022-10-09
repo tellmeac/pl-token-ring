689cadbb29a058ab915c9c4545636781c966097d
Alexander Lipatov <tellmeac@gmail.com>
2022-10-09 16:45:26 +0700

    feat: api to post tokens

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 // Copyright 2022 Lipatov Alexander
-//
+
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+
 // 	http://www.apache.org/licenses/LICENSE-2.0
-//
+
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,122 +15,45 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
-	"time"
+	"net/http"
 )
 
-type Token struct {
-	Data     string
-	Reciever uint
-	TTL      uint
+type Server struct {
+	Target chan Token
 }
 
-type Node struct {
-	id      uint
-	beforeC <-chan Token
-	nextC   chan Token
-
-	doneC <-chan bool
-}
-
-func (node *Node) Run() {
-	go func() {
-		for {
-			select {
-			case t := <-node.beforeC:
-				switch {
-				case t.Reciever == node.id:
-					log.Printf("Token has been accepted by %d; message: %s (with left ttl = %d)", t.Reciever, t.Data, t.TTL)
-				case t.TTL > 0:
-					t.TTL -= 1
-					node.nextC <- t
-				default:
-					log.Printf("Token is expired (%+v))", t)
-				}
-			case <-node.doneC:
-				return
-			}
+func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		var token Token
+		if err := json.NewDecoder(r.Body).Decode(&token); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
 		}
-	}()
-}
 
-type TokenRing struct {
-	doneC <-chan bool
-	nodes []*Node
-}
+		s.Target <- token
 
-func NewTokenRing(size uint, doneC <-chan bool) *TokenRing {
-	tr := &TokenRing{
-		doneC: doneC,
-		nodes: make([]*Node, 0, size),
+		w.WriteHeader(http.StatusOK)
 	}
-
-	if size < 2 {
-		log.Fatal("Expected to have at least 2 size of ring")
-	}
-
-	firstNode := &Node{
-		id:    0,
-		nextC: make(chan Token),
-		doneC: doneC,
-	}
-	tr.nodes = append(tr.nodes, firstNode)
-
-	var i uint
-	for i = 1; i < size; i++ {
-		tr.nodes = append(tr.nodes, &Node{
-			id:      i,
-			beforeC: tr.nodes[i-1].nextC,
-			nextC:   make(chan Token),
-			doneC:   doneC,
-		})
-	}
-	// Assign firstNode beforeC from last node
-	firstNode.beforeC = tr.nodes[size-1].nextC
-
-	return tr
-}
-
-func (tr *TokenRing) Run() chan Token {
-	for _, node := range tr.nodes {
-		node.Run()
-	}
-	return tr.nodes[len(tr.nodes)/2].nextC
 }
 
 func main() {
-	var chainSize = flag.Uint("n", 12, "Chain size")
+	var chainSize = flag.Int("n", 12, "Chain size")
 	flag.Parse()
-
 	if chainSize == nil {
-		log.Fatal("Expected to provide n uint value, number of nodes in token ring")
+		log.Fatal("Expected to provide int value, number of nodes in a token ring")
 	}
 
-	done := make(chan bool)
+	tRing := NewTokenRing(*chainSize)
 
-	tr := NewTokenRing(*chainSize, done)
-
-	sendC := tr.Run()
-
-	sendC <- Token{
-		Data:     "Hello World",
-		Reciever: 1,
-		TTL:      1000,
+	server := Server{
+		Target: tRing.Run(),
 	}
 
-	sendC <- Token{
-		Data:     "Undelivered, in some case it could be delivered :)",
-		Reciever: 1000,
-		TTL:      9999,
+	if err := http.ListenAndServe(":8080", server); err != nil {
+		log.Fatalf("Server failed: %s", err)
 	}
-
-	sendC <- Token{
-		Data:     "Too short to live",
-		TTL:      0,
-		Reciever: 0,
-	}
-
-	time.Sleep(10 * time.Second)
-	done <- true
 }
---
+++ token_ring.go
@@ -0,0 +1,91 @@
+// Copyright 2022 Lipatov Alexander
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// 	http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import "log"
+
+// Token is a message type.
+type Token struct {
+	Data     string `json:"data"`
+	Reciever int    `json:"reciever"`
+	TTL      int    `json:"ttl"`
+}
+
+type TokenRing struct {
+	Nodes []*Node
+}
+
+func NewTokenRing(size int) *TokenRing {
+	tr := &TokenRing{
+		Nodes: make([]*Node, 0, size),
+	}
+
+	if size < 2 {
+		log.Fatal("Expected to have at least 2 size of ring")
+	}
+
+	firstNode := &Node{
+		ID:    0,
+		NextC: make(chan Token),
+	}
+	tr.Nodes = append(tr.Nodes, firstNode)
+
+	var i int
+	for i = 1; i < size; i++ {
+		tr.Nodes = append(tr.Nodes, &Node{
+			ID:      i,
+			BeforeC: tr.Nodes[i-1].NextC,
+			NextC:   make(chan Token),
+		})
+	}
+	// Assign firstNode beforeC from last node
+	firstNode.BeforeC = tr.Nodes[size-1].NextC
+
+	return tr
+}
+
+func (tr *TokenRing) Run() chan Token {
+	for _, node := range tr.Nodes {
+		go node.Run()
+	}
+	return tr.Nodes[len(tr.Nodes)/2].NextC
+}
+
+// Node represents a single block in a ring.
+type Node struct {
+	ID int
+
+	BeforeC <-chan Token
+	NextC   chan Token
+}
+
+// Run runs blocking listening for token.
+func (node *Node) Run() {
+	for t := range node.BeforeC {
+		node.process(t)
+	}
+}
+
+func (node *Node) process(t Token) {
+	switch {
+	case t.Reciever == node.ID:
+		log.Printf("Token has been accepted by %d; message: %s (with left ttl = %d)", t.Reciever, t.Data, t.TTL)
+	case t.TTL > 0:
+		t.TTL -= 1
+		node.NextC <- t
+	default:
+		log.Printf("Token for %d is expired", t.Reciever)
+	}
+}
